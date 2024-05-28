9243e2e918e55b112c4adbd6670a479154b73eba
plaze <banikakash131@gmail.com>
2024-05-28 17:46:49 +0530

    feat(routes): add auth routes

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ import colors from "colors";
 import morgan from "morgan";
 
 import { connectDB } from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
 
 //database
 connectDB();
@@ -21,7 +22,10 @@ app.use(morgan("dev"));
 app.get("/", async (req, res) => {
   return res.status(200).send("hello");
 });
+//routes
+app.use("/api/v1/auth", authRoute);
 
+//listen
 mongoose.connection.once("open", () => {
   app.listen(PORT, () => {
     console.log(`server is listenning in PORT ${PORT}`.bgBlue);
