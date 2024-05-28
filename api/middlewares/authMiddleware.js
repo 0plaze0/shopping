3abd573cd18a37408cc8d0615c26327936f240ea
plaze <banikakash131@gmail.com>
2024-05-28 21:59:32 +0530

    feat: add admin middleware

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 export const requireSignIn = async (req, res, next) => {
   try {
@@ -11,5 +12,28 @@ export const requireSignIn = async (req, res, next) => {
     next();
   } catch (err) {
     console.log(err);
+    res.status(500).send({
+      success: false,
+      err,
+      message: "Error in token validation middleware",
+    });
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (user.role !== 1) {
+      return res
+        .status(401)
+        .send({ success: false, message: "Unauthorized Access" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .send({ success: false, err, message: "Error in Admin middleware" });
   }
 };
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import authController from "../controllers/authController.js";
-import { requireSignIn } from "./../middlewares/authMiddleware.js";
+import { requireSignIn, isAdmin } from "./../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/register").post(authController.registerController);
 router.route("/login").post(authController.loginController);
-router.route("/test").get(requireSignIn, async (req, res) => {
+router.route("/test").get(requireSignIn, isAdmin, async (req, res) => {
   return res.send("hello");
 });
 
