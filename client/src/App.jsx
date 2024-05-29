f7748f39d917d830b586aa8464f29ad0a0ea0cb8
plaze <banikakash131@gmail.com>
2024-05-29 20:15:11 +0530

    feat(components): add layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import { Layout } from "./components";
 const App = () => {
-  return <div className="app">E-commerce application</div>;
+  return (
+    <div className="app">
+      <Layout>
+        <h1>E-commerce application</h1>
+      </Layout>
+    </div>
+  );
 };
 
 export default App;
