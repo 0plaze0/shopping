7f9f092d1b5cdbfd2472b4a2995903960eca025a
plaze <banikakash131@gmail.com>
2024-05-30 17:25:25 +0530

    feat: add auth context

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import { SEO } from "./../components";
+import { useAuth } from "../context/auth";
 
 const HomePage = () => {
+  const [auth, setAuth] = useAuth();
   return (
     <div>
       <SEO />
       <h1>Homepage</h1>
+      <pre>{JSON.stringify(auth, null, 4)}</pre>
     </div>
   );
 };
