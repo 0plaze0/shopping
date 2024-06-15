63249d4a670c1fe335bc7fa3587ed0cd931fccd5
plaze <banikakash131@gmail.com>
2024-06-15 22:23:08 +0530

    feat: add search context

---
+++ client/src/context/search.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, useContext, createContext } from "react";
+
+const SearchContext = createContext();
+
+const SearchProvider = ({ children }) => {
+  const [search, setSearch] = useState({
+    keyword: "",
+    results: [],
+  });
+
+  //default send token with every request
+
+  return (
+    <SearchContext.Provider value={[search, setSearch]}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+const useSearch = () => useContext(SearchContext);
+
+export { useSearch, SearchProvider };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/auth";
+import { SearchProvider } from "./context/search";
 
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <SearchProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </SearchProvider>
   </React.StrictMode>
 );
